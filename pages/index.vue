840e0fd426693fd48f493a68263d83d6a85efa4b
gmustiere <gmustiere@groupe-gemy.fr>
2020-09-24 16:36:05 +0200

    :art: ajout contenu rédactionnel

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <Presentation />
     <Services />
     <Contact />
+    <Fournisseur />
     <Footer />
   </div>
 </template>
@@ -14,9 +15,11 @@ import Header from '@/components/Header.vue'
 import Presentation from '@/components/Ambition.vue'
 import Services from '@/components/Services.vue'
 import Contact from '@/components/Contact.vue'
+import Fournisseur from '@/components/Fournisseur.vue'
 
 export default {
   components: {
+    Fournisseur,
     Contact,
     Services,
     Presentation,
