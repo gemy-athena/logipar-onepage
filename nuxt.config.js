680b931ea18c85d70bd8e3f3d01272850b679809
Sébastien Talbot <stalbot@groupe-gemy.fr>
2020-09-25 16:59:13 +0200

    update SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,14 @@ export default {
   ssr: false,
   target: 'static',
   head: {
-    title: 'Logipar',
+    title: 'Logipar - Distributeur de pièces de rechange automobile multimarques',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Logipar' }
+      { hid: 'description',
+        name: 'description',
+        content: 'Le spécialiste de la distribution de pièces de rechange automobile multimarques. Logipar opère au sein du réseau Distrigo depuis ses 2 plateformes : Ouest (Torcé) & Provence (Le Luc).'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
