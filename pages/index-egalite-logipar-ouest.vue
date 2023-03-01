595fd0d30115ef35cf172fecb9d3a205caab59e4
gmustiere <mustiere.gabriel@gmail.com>
2023-03-01 10:34:41 +0100

    Mise à jour index égalité logipar ouest

--- pages/index-egalite-logipar-ouest.vue
+++ pages/index-egalite-logipar-ouest.vue
@@ -8,42 +8,27 @@
         </div>
 
         <p class="my-2">
-          L’index de l’égalité femmes-hommes est entré en vigueur depuis le 1er mars 2019 suite à la loi « Avenir Professionnel » du 5 septembre 2018 qui a pour
-          ambition de promouvoir l’égalité professionnelle entre les femmes et les hommes à travers 5 indicateurs, sur une échelle de 100. Les 5 indicateurs
-          sont les suivants :
+          L’Index de l'égalité professionnelle a été conçu pour faire progresser au sein des entreprises l’égalité salariale entre les femmes et les hommes.
+        </p>
+
+        <p class="my-2">
+          Il permet aux entreprises de mesurer, en toute transparence, les écarts de rémunération entre les sexes et de mettre en évidence leurs points de progression. Lorsque des disparités salariales sont constatées, des mesures de correction doivent être prises.
+        </p>
+
+        <p class="my-2">
+          La note globale obtenue pour l’index égalité femmes-hommes de 2022 est de 82/100.
+        </p>
+
+        <p class="my-6">
+          Les résultats de l’index égalité femmes – hommes relatifs à l’année 2022 pour <strong>Logipar Ouest</strong> sont les suivants :
         </p>
 
         <div class="flex justify-center">
           <ul class="list-disc my-6">
-            <li>Indicateur 1 : écart de rémunération H/F,</li>
-            <li>Indicateur 2 : écart de taux d’augmentations individuelles H/F,</li>
-            <li>Indicateur 3 : écart de taux de promotion H/F,</li>
-            <li>Indicateur 4 : taux de salariées augmentées après leur retour de congé maternité,</li>
-            <li>Indicateur 5 : répartition sexuée des dix salariés ayant les plus hautes rémunérations.</li>
-          </ul>
-        </div>
-
-        <div class="my-6 flex justify-center">
-          <LogoTitre class="object-cover h-12" />
-        </div>
-
-        <p class="my-6">
-          L’égalité femmes-hommes est aujourd’hui un enjeu sociétal majeur. Nous nous engageons à garantir un équilibre sur la promotion, la rémunération et les
-          augmentations des salariés FEMMES-HOMMES. Logipar Ouest est déterminé à maintenir ses efforts en matière de mixité professionnelle.
-        </p>
-
-        <p class="my-6">
-          Les résultats de l’index égalité femmes – hommes relatifs à l’année 2021 pour Logipar Ouest sont les suivants :
-        </p>
-
-        <div class="flex justify-center">
-          <ul class="list-disc my-6">
-            <li>Indicateur 1 : 21 points</li>
-            <li>Indicateur 2 : 20 points</li>
-            <li>Indicateur 3 : 15 points</li>
-            <li>Indicateur 4 : 15 points</li>
-            <li>Indicateur 5 : 5 points</li>
-            <li>Note globale : 76 points</li>
+            <li>Indicateur 1 : écart de rémunération : 37/40</li>
+            <li>Indicateur 2 : écart de taux d’augmentations individuelles : 25/35</li>
+            <li>Indicateur 3 : taux de salariées augmentées après leur retour de congé maternité : 15/15</li>
+            <li>Indicateur 4 : nombre de salariés du sexe sous représenté parmi les 10 plus hautes rémunérations : 5/10</li>
           </ul>
         </div>
       </div>
